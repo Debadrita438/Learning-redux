83157421be61d10941f4f3bc429f02e995ce077b
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2021-10-21 14:54:41 +0530

    Creating a new action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,71 +1,41 @@
-import { Component } from 'react';
-
-import { connect, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
-// const Counter = () => {
-//     const counter = useSelector(state => state.counter);
-//     const dispatch = useDispatch();
+const Counter = () => {
+    const counter = useSelector(state => state.counter);
+    const toggleCounter = useSelector(state => state.showCounter);
+    const dispatch = useDispatch();
 
-//     const incrementHandler = () => {
-//         dispatch({ type: 'INCREMENT' })
-//     }
-
-//     const decrementHandler = () => {
-//         dispatch({ type: 'DECREMENT' })
-//     }
-//     const toggleCounterHandler = () => {};
-
-//     return (
-//         <main className={classes.counter}>
-//             <h1>Redux Counter</h1>
-//             <div className={classes.value}>{counter}</div>
-//             <div>
-//                 <button onClick={incrementHandler}>Increment</button>
-//                 <button onClick={decrementHandler}>Decrement</button>
-//             </div>
-//             <button onClick={toggleCounterHandler}>Toggle Counter</button>
-//         </main>
-//     );
-// };
-
-const mapStateToProps = state => {
-    return {
-        counter: state.counter
-    }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        increment: () => dispatch({ type: 'INCREMENT' }),
-        decrement: () => dispatch({ type: 'DECREMENT' })
-    }
-}
-
-class Counter extends Component { 
-    incrementHandler = () => {
-        this.props.increment();
+    const incrementHandler = () => {
+        dispatch({ type: 'INCREMENT' })
     }
 
-    decrementHandler = () => {
-        this.props.decrement();
+    const increaseHandler = () => {
+        dispatch({
+            type: 'INCREASE',
+            payload: 10
+        })
     }
 
-    toggleCounterHandler = () => {}
-
-    render() {
-        return (
-            <main className={classes.counter}>
-                <h1>Redux Counter</h1>
-                <div className={classes.value}>{this.props.counter}</div>
-                <div>
-                    <button onClick={this.incrementHandler}>Increment</button>
-                    <button onClick={this.decrementHandler}>Decrement</button>
-                </div>
-                <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-         </main>
-        )
+    const decrementHandler = () => {
+        dispatch({ type: 'DECREMENT' })
     }
-}
+    const toggleCounterHandler = () => {
+        dispatch({ type: 'TOGGLE' })
+    };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+    return (
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            {toggleCounter && <div className={classes.value}>{counter}</div>}
+            <div>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={increaseHandler}>Increase By 10</button>
+                <button onClick={decrementHandler}>Decrement</button>
+            </div>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+    );
+};
+
+export default Counter;
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux';
 
 const initialState = {
-    counter: 0
+    counter: 0,
+    showCounter: true
 }
 
 const counterReducer = (state = initialState, action) => {
@@ -11,10 +12,20 @@ const counterReducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter + 1
             }
+        case 'INCREASE':
+            return {
+                ...state,
+                counter: state.counter + action.payload
+            }
         case 'DECREMENT': 
             return {
                 ...state,
                 counter: state.counter - 1
+            }
+        case 'TOGGLE':
+            return {
+                ...state,
+                showCounter: !state.showCounter
             }
         default:
             return state;
